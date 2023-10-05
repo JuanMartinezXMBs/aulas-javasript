545a34d526795e9992717a3e21c0ae28dadacd55
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-05 15:29:16 -0300

    Update 02-estruturas.js

--- js/02-estruturas.js
+++ js/02-estruturas.js
@@ -1,1 +1,10 @@
-console.log("Exemplo 2");
+/* Estruturas de Dados 
+
+Arrays (Vetores/Matrizes):
+Lista de dados indexados, sequenciais acessiveis por um indice numérico.
+
+Objetos: 
+Lista de dados não indexados e formados por propriedados e valores.*/
+
+// Arrays
+let cursos = ["Node.js", "React", "SQL", "UX/UI Desing"];

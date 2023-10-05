3550f5193902b3007725b9b9173714343376fdc8
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-05 15:04:05 -0300

    Explicação JS
    texplicando "tags" primarias com resultados no console do dev tool

---
+++ js/01-sintaxe.js
@@ -0,0 +1,34 @@
+console.log("programador WEB");
+console.log("Senac Penha");
+
+/* comentario ALT SHIFT A */
+
+// COMENTARIO CTRL + :
+
+/* MANIPULAÇÃO DE DADOS */
+
+// Variáveis let = tag para variaveis
+let curso = "HTML5 E CSS3";
+let ano = 2023;
+
+// Contantes
+const mediaMinima = 7;
+
+// Saida de dados apartir da memoria
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+/* Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023 */
+
+// texto ou numeros dentro das crases (`text`) dados estatico
+// ${} = dados dinamicos
+
+// template Strimg/Literal: Saida usando crases e ${}
+console.log(`Estou fazendo o curso de ${curso} neste ano de ${ano}.`);
+
+
+// concatenação (forma mais antiga, mas ainda válida)
+// + = usaddo para juntar e não somar elementos
+console.log("estou fazendo o curso de "+curso+" neste ano de "+ano+".");
+

afd9d88e0a8b13a3cd0394dfbd53f7126a6c5224
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-10 17:27:00 -0300

    Update exercicio01.js

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -1,3 +1,7 @@
+/* Habilitando o modo estrito do JS */
+
+"use strict";
+
 let nota01 = 7;
 let nota02 = 7;
 
@@ -13,3 +17,28 @@ if (media >= 7){
 }
 
 console.log(`o aluno está: ${mensagem}`);
+
+console.log(`--------------------------------------------------------`);
+
+let ponto = 0;
+let pontolevados = 15;
+
+let soma = (ponto + pontolevados);
+let mensagemDetran;
+
+console.log(soma);
+
+if (soma >= 7) {
+    mensagemDetran = "tome mais cuidado";
+}
+else if (soma >= 10) {
+    mensagemDetran = "tome mais cuidado com a sua direção, você pode ter a sua CNH caçada";
+}
+else if (soma >= 13) {
+    mensagemDetran = "sua Carteira Nacional de Habilitação (CNH) está caçada! procure uma agência mais proxima e regularizar sua situação";
+}
+else if (soma >= 15) {
+    mensagemDetran = "sua Carteira Nacional de Habilitação (CNH) está sendo caçada!";
+}
+
+console.log(`Detran: ${mensagemDetran}`)

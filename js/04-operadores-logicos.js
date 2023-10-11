7dc8d0e9aab49e08b141100ae3e7e28a0443df9d
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-11 14:25:03 -0300

    notas aluno

---
+++ js/04-operadores-logicos.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Requisitos da escola para aprovação
+const mediaMinima = 7;
+const limiteDeFaltas = 10;
+
+// Resultados do aluno
+let aluno = "jean";
+let mediaFinal = 8.5;
+let faltas = 8;
+let resultado;
+
+/* Operador logico E (and) */
+// if( mediaFinal >= mediaMinima && faltas <= limiteDeFaltas ){
+//     resultado = "aprovado";
+// } else {
+//     resultado = "reprovado"
+// }
+
+
+
+console.log(`O aluno ${aluno} está ${resultado}`);
+console.log(`Media do aluno: ${mediaFinal}`);
+console.log(`Faltas do aluno: ${faltas}`);

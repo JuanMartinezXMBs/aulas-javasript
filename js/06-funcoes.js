4b8f712f8bdf6450777b93f60648be89652432c7
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-17 14:53:54 -0300

    text explicativo

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -1,3 +1,11 @@
 "use strict";
 
-console.log("Funções!");
+/* Sobre Funções:
+Funções são blocos de codigos que podem ser reaproveitados em praticamente qualquer lugar de uma aplicação
+
+Usar Funções também é benefico para o ponto de viista de organização do código
+
+O JS possui centanas de funções prontas:
+log(), alert(), toFixed(2), prompt etc...
+*/
+

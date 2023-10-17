ed9fff7b536c0ff5d82daf815b489a206420ff4d
JuanMartinezXMBs <juanmllmz@gmail.com>
2023-10-17 15:19:37 -0300

    primeiro ex de função

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -9,3 +9,35 @@ O JS possui centanas de funções prontas:
 log(), alert(), toFixed(2), prompt etc...
 */
 
+//Forma 1: função anonima
+const exemplo1 = function(){
+    console.log("Função anônima");
+};
+
+//Forma 2: Função nomeada/declarada
+function exemplo2(){
+    console.log("Função nomeada/declarada");
+};
+
+//Forma 3: arrow Function
+const exemplo3 = () => {
+    console.log("Arrow Function");
+};
+
+//CHAMADA DE FUNÇÃO
+exemplo1();
+exemplo2();
+exemplo3();
+
+/* Exemplos diversos */
+
+//Função com parâmetro e retorno de dados/resultados
+function somar(valor1, valor2){
+    return valor1 + valor2;
+}
+
+/* Chamamos a função diversas vezes e passando valores diferentes como parâmetros. */
+console.log( somar(10,20));
+console.log( somar(10,20));
+console.log( somar(10,20));
+
